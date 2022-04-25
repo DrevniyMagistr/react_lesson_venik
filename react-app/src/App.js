f24ca1a1275c33793a6ff9fb786b88e387eff4ec
DrevniyMagistr <sasha.klishch@gmail.com>
2022-04-25 20:50:30 +0300

    added button for test

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('Title');
   return (
     <div className="App">
       <header className="App-header">
+        <button onClick={() => {
+          setTitle(1000)
+        }}>Click Me</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
